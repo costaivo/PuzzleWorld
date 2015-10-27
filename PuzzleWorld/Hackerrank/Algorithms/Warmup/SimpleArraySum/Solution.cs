fac2239e6a228cfdb6afd4795a4f60a4655c1fea
Ivo Costa <ivo@creativecapsule.com>
2015-10-28 00:08:57 +0530

    Added new  puzzles

---
+++ PuzzleWorld/Hackerrank/Algorithms/Warmup/SimpleArraySum/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PuzzleWorld.Hackerrank.Algorithms.Warmup.SimpleArraySum
+{
+   public  class Solution
+    {
+       public static void GetSolution()
+       {
+           Console.WriteLine("Very Simple Sum");
+           var rawData = Console.ReadLine();
+           int noOfIntergers;
+           int.TryParse(rawData, out  noOfIntergers);
+           rawData = Console.ReadLine();
+           int sum = 0;
+           foreach (string s in rawData.Split(' '))
+           {
+               int currentValue;
+               int.TryParse(s, out currentValue);
+               sum += currentValue;
+           }
+           Console.WriteLine("Sum " + sum);
+       }
+    }
+}
--- PuzzleWorld/Program.cs
+++ PuzzleWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,19 @@ namespace PuzzleWorld
    public class Program
     {
         public static void Main(string[] args)
-        {
-            // Equilibrium Problem
-            Console.WriteLine("Equilibrium Problem");
-            int[] inputArray = new int[] { -1, 1, 0 };
-            int equilibriumPosition = Equilibrium.Solution.GetEquilibriumPosition(inputArray);
+       {
+           // #region Equilibrium Problem
+           // Console.WriteLine("Equilibrium Problem");
+           // int[] inputArray = new int[] { -1, 1, 0 };
+           // int equilibriumPosition = Equilibrium.Solution.GetEquilibriumPosition(inputArray);
 
-            Console.WriteLine(equilibriumPosition.ToString());
-            Console.ReadLine();
-        }
+           // Console.WriteLine(equilibriumPosition.ToString());
+           // Console.ReadLine();
+           //#endregion
+
+
+            Hackerrank.Algorithms.Warmup.SimpleArraySum.Solution.GetSolution();
+            Console.ReadKey();
+       }
     }
 }
