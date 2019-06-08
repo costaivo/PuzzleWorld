ff25ea340dcf210a819933f0df098485cd0aa53a
Ivo Costa <costa_ivo@yahoo.com>
2019-06-09 01:00:18 +0530

    Updated solution for 001.js

--- project-euler/001/001.js
+++ project-euler/001/001.js
@@ -1,6 +1,6 @@
 
 /*************************************************************
- *  Optimized  Solution
+ *  Optimized  Solution 1
 -*************************************************************/
 function arithmicSum(n) {
     return (n * (n + 1)) / 2
@@ -13,5 +13,23 @@ function arithmicSequence(limit)  {
 	const fifteens = 15 * arithmicSum(Math.floor(limit/15));
 	return parseInt(threes + fives - fifteens)
 }
+let n =100
+console.log(arithmicSequence(n))
 
-console.log(arithmicSequence(1000))
+/*************************************************************
+ *  Optimized  Solution 2
+-*************************************************************/
+
+function sumDivisbleBy( n,  p){
+    const  sum = n * (parseInt(p / n)) * ((parseInt(p / n)) + 1) / 2;
+    return sum;
+}
+
+function sumOfNumbersDivisbleBy(num1,num2,n)
+{
+    n=n-1
+    return  sumDivisbleBy(num1,n)+sumDivisbleBy(num2,n)-sumDivisbleBy(num1*num2,n);
+}
+
+
+console.log(sumOfNumbersDivisbleBy(3,5,n));
