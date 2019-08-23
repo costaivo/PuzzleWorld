c15530f0fc978889dfa1b7bde184ac74c55e49a9
Ivo Costa <costa_ivo@yahoo.com>
2019-08-23 22:07:17 +0530

    kotlin hello world

--- project-euler/001/001.kt
+++ project-euler/001/001.kt
@@ -1,5 +1,8 @@
-fun main(args:Array<String>){
-    
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
 }
-
-
