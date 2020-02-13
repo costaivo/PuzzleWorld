7de322ac7429e22cb525ff87573c0a20bf70f3c3
Ivo Costa <costaivo@users.noreply.github.com>
2020-02-13 12:10:43 +0530

    Update stepsToReduceToZero.py

--- easy/stepsToReduceToZero.py
+++ easy/stepsToReduceToZero.py
@@ -14,3 +14,15 @@ class Solution:
             num = num // 2
         
         return steps
+
+# Normal Solution
+   def numberOfSteps_2 (self, num: int) -> int:
+        count = 0
+        while num >0:
+            if (num % 2) == 0:
+                num = num/2
+                count += 1
+            else:
+                num = num -1
+                count +=1
+        return count
