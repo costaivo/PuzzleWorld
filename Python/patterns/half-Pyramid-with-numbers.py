a75ff92b25130bd0f6d945cf2a995e7a67bd688d
Ivo Costa <costaivo@users.noreply.github.com>
2022-04-07 22:10:06 +0530

    clean up

--- Python/patterns/half-Pyramid-with-numbers.py
+++
@@ -1,5 +0,0 @@
-count = int(input('Enter the number of columns you want to print in the last row? --> '))
-for i in range(1, count + 1):
-    for j in range(1, i + 1):
-        print(j,end=' ')
-    print('')
